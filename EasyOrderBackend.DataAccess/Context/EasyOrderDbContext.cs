8103b6663fbe11e0996d5483c387b144604eca26
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-11-18 02:18:21 +0300

    Create DataAccess project (resolves #4)

---
+++ EasyOrderBackend.DataAccess/Context/EasyOrderDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyOrderBackend.DataAccess.Context
+{
+    class EasyOrderDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            //string connectionString = @"Data Source=localhost\SQLEXPRESS;Database=EasyOrderDb;Integrated Security=True";
+            //optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
---
+++ EasyOrderBackend.DataAccess/Repositories/Concrete/EasyOrderCodeRepository.cs
@@ -0,0 +1,13 @@
+﻿using EasyOrderBackend.DataAccess.Repositories.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyOrderBackend.DataAccess.Repositories.Concrete
+{
+    public class EasyOrderCodeRepository : IEasyOrderCodeRepository
+    {
+    }
+}
