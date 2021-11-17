0a2a23f4689a83f43883f3377900facf9711ec7d
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-11-18 02:05:31 +0300

    Create Business project (resolves #3)

---
+++ EasyOrderBackend.Business/Services/Concrete/EasyOrderCodeService.cs
@@ -0,0 +1,13 @@
+﻿using EasyOrderBackend.Business.Services.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyOrderBackend.Business.Services.Concrete
+{
+    class EasyOrderCodeService : IEasyOrderCodeService
+    {
+    }
+}
