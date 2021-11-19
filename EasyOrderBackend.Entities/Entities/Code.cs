aa3fc4357f037105b013c2d1fb485d5dd88d8fb9
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-11-19 23:40:49 +0300

    Delete EasyOrderCode entity, Add Code entity (resolves #16)

---
+++ EasyOrderBackend.Entities/Entities/Code.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyOrderBackend.Entities.Entities
+{
+    public class Code
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [StringLength(32, MinimumLength = 3, ErrorMessage = "Maximum 32, Minimum 3 Characters")]
+        [DataType(DataType.Text)]
+        public string Title { get; set; }
+
+        [StringLength(32, MinimumLength = 3, ErrorMessage = "Maximum 32, Minimum 3 Characters")]
+        [DataType(DataType.Text)]
+        public string ContactTitle { get; set; }
+
+        [StringLength(32, MinimumLength = 3, ErrorMessage = "Maximum 32, Minimum 3 Characters")]
+        [DataType(DataType.Text)]
+        public string AddressTitle { get; set; }
+
+        [StringLength(32, MinimumLength = 3, ErrorMessage = "Maximum 32, Minimum 3 Characters")]
+        [DataType(DataType.Text)]
+        public string CardTitle { get; set; }
+
+        public Guid GuidCode { get; set; }
+
+        public bool IsRecordValid { get; set; }
+
+        public int InsertUserId { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime InsertDateTime { get; set; }
+
+        public int UpdateUserId { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime UpdateDateTime { get; set; }
+    }
+}
